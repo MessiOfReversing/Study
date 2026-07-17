957c48e34e81540fd8ac3175fef6427d8eda91c6
gurd <justchatting1213@gmail.com>
2026-07-17 13:32:11 +0900

    Temp

---
+++ Rocq.v
@@ -0,0 +1,10 @@
+Definition next_working_day (d:day) : day :=
+  match d with
+  | monday ⇒ tuesday
+  | tuesday ⇒ wednesday
+  | wednesday ⇒ thursday
+  | thursday ⇒ friday
+  | friday ⇒ monday
+  | saturday ⇒ monday
+  | sunday ⇒ monday
+  end.
